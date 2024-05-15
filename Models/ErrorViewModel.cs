1311c2aa02689c2e99477723cce2e3352cda30d6
MalindaGamage <pkgmalinda@gmail.com>
2024-05-15 08:52:45 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplicationForm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
