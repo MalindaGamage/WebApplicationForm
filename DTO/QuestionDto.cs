c0208ba2624050f4e09a13447ee23798903baab8
MalindaGamage <pkgmalinda@gmail.com>
2024-05-15 09:13:48 +0530

    Question and Question DTO classes

---
+++ DTO/QuestionDto.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplicationForm.DTO
+{
+    public class QuestionDto
+    {
+        public string? Id { get; set; }
+        public string Content { get; set; }
+        public string Type { get; set; } // "Paragraph", "YesNo", "Dropdown", "MultipleChoice", "Date", "Number"
+        public List<string> Options { get; set; } // For Dropdown and MultipleChoice
+        public int MinValue { get; set; } // For Number
+        public int MaxValue { get; set; } // For Number
+        public string Placeholder { get; set; } // For Paragraph
+    }
+}
---
+++ Models/Question.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace WebApplicationForm.Models
+{
+    public class Question
+    {
+        [JsonProperty("id")]
+        public string? Id { get; set; }
+
+        [JsonProperty("content")]
+        public string Content { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; } // "Paragraph", "YesNo", "Dropdown", "MultipleChoice", "Date", "Number"
+
+        [JsonProperty("options")]
+        public List<string> Options { get; set; } = new List<string>();
+
+        [JsonProperty("minValue")]
+        public int MinValue { get; set; }
+
+        [JsonProperty("maxValue")]
+        public int MaxValue { get; set; }
+
+        [JsonProperty("placeholder")]
+        public string Placeholder { get; set; }
+    }
+}
